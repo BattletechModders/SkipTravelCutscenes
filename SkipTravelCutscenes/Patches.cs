98ec463a72443ade1e71707543d5e77997083b9d
Michael P. Starkweather <mpstark@gmail.com>
2019-01-05 00:02:44 -0600

    Add mod.json, remove assemblies that don't matter, check versioning to use wildcard

--- SkipTravelCutscenes/Patches.cs
+++ SkipTravelCutscenes/Patches.cs
@@ -1,10 +1,6 @@
 ﻿using BattleTech;
 using Harmony;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
 
 namespace SkipTravelCutscenes
 {
