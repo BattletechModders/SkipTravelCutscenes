b03392f1faa3c8c8c3e6f998be9612b0d4b0b144
Michael P. Starkweather <mpstark@gmail.com>
2019-01-04 23:45:13 -0600

    First attempt at simple patch

---
+++ SkipTravelCutscenes/Patches.cs
@@ -0,0 +1,28 @@
+﻿using BattleTech;
+using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace SkipTravelCutscenes
+{
+    [HarmonyPatch(typeof(SGTravelManager), "BeginTransferAnim")]
+    public static class SGTravelManager_BeginTransferAnim_Patch
+    {
+        public static void Postfix(SGTravelManager __instance)
+        {
+            __instance.AnimationInterrupt();
+        }
+    }
+
+    public static class Patches
+    {
+        public static void Init(string modDir, string modSettings)
+        {
+            var harmony = HarmonyInstance.Create("io.github.mpstark.SkipTravelCutscenes");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
